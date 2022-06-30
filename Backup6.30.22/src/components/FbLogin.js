f51b54af3c5d789f30dca6ea7ba6697efeb5468d
Vassardog77 <66711902+Vassardog77@users.noreply.github.com>
2022-06-29 22:53:19 -0700

    Add files via upload

---
+++ Backup6.30.22/src/components/FbLogin.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import FacebookLogin from "react-facebook-login"
+import '../App.css';
+import FbPosts from './FbPosts';
+import IgSelect from './IgSelect';
+import SideBar from './SideBar';
+
+function FbLogin() {
+
+    const [isLoggedIn, setisLoggedIn] = useState(false)
+    /*const [userID, setuserID] = useState("")
+    const [name, setname] = useState("")
+    const [email, setemail] = useState("")
+    const [picture, setpicture] = useState("")*/
+    const [accessToken, setaccessToken] = useState("")
+
+
+    const responseFacebook = response => {
+            //console.log(response);
+            setisLoggedIn(true)                 //UNCOMMENT THIS LINE TO ALLOW LOGIN
+            /*setuserID(response.userID)
+            setname(response.name)
+            setemail(response.email)
+            setpicture(response.picture.data.url)*/
+            setaccessToken(response.accessToken)
+            }
+
+        let fbContent;
+
+        if (isLoggedIn === (true)) {              //add fb posts once logged in
+          fbContent = (<div></div>
+          )
+        } else {
+          fbContent = (
+            <FacebookLogin
+              appId="354529376664526"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope='publlic_profile, publish_to_groups,
+               groups_show_list, instagram_basic, pages_show_list'
+              callback={responseFacebook}
+            />
+
+            );
+        }
+
+    return (
+      <div>
+        <div id='fblogin_button'>
+          <div>
+            {fbContent}
+          </div>
+          <SideBar accessToken = {accessToken} id='sidebar'></SideBar>
+        </div>
+      </div>
+    );
+}
+
+export default FbLogin;
---
+++ Backup6.30.22/src/components/SideBar.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import FbSchedule from './FbSchedule';
+import Gmail from './Gmail';
+import IgSelect from './IgSelect';
+
+function SideBar(props) {
+
+    let FbToken = props.accessToken
+    let token = localStorage.getItem('data')
+
+
+    return (
+        <div className='menu'>
+            <li className='menu-item' id='mail'>
+                <a href='#mail' className='btn'>
+                    Send Emails    
+                </a> 
+                <div className='menu-item__sub'>
+                    <div>
+                        <Gmail></Gmail>
+                    </div>
+                </div>
+            </li>
+            <li className='menu-item' id='fb'>
+                <a href='#fb' className='btn'>
+                    Schedule Facebook Posts    
+                </a> 
+                <div className='menu-item__sub'>
+                    <div>
+                        <FbSchedule></FbSchedule>
+                    </div>
+                </div>
+            </li>
+            <li className='menu-item' id='insta'>
+                <a href='#insta' className='btn'>
+                    Schedule Instagram Post    
+                </a> 
+                <div className='menu-item__sub'>
+                    <div>
+                        <IgSelect FbToken = {FbToken}></IgSelect>
+                    </div>
+                </div>
+            </li>
+            <li className='menu-item' id='close'>
+                <a href='#close' className='btn'>
+                    Close    
+                </a> 
+            </li>
+        </div>
+    );
+}
+
+export default SideBar;
